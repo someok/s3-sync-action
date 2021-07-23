54e754b881204fb7ca5be7320d4c83b698781cda
JX Wang <wjianxu@gmail.com>
2021-07-23 23:28:00 +0800

    feat: 更换 docker 配置

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,8 @@ if [ -n "$AWS_S3_ENDPOINT" ]; then
   ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
 fi
 
+aws -V
+
 # Create a dedicated profile for this action to avoid conflicts
 # with past/future actions.
 # https://github.com/jakejarvis/s3-sync-action/issues/1
